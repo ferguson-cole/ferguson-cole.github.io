038c043632f709ac07a0144d9f401a0b63e1d559
Cole <coleferguson9@gmail.com>
2021-03-03 12:55:59 -0800

    added javascript navbar

---
+++ navbar.js
@@ -0,0 +1,11 @@
+document.write('\
+    <nav class="navbar">\
+        <div>\
+            <li><a class="active" href="index.html">Home</a></li>\
+            <li><a href="about.html">About Me</a></li>\
+            <li><a href="resume.html">Resume</a></li>\
+            <li><a href="https://github.com/ferguson-cole" target="_blank" rel="noopener noreferrer">Github</a></li>\
+            <li><a href="contact.html">Contact</a></li>\
+        </div>\
+    </nav>\
+');
