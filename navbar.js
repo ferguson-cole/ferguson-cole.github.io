eaab5ea454ec49303b3b4b1d705a902bc56aead7
Cole <coleferguson9@gmail.com>
2021-03-03 13:47:01 -0800

    tweaked navbar attributes

--- navbar.js
+++ navbar.js
@@ -1,11 +1,11 @@
 document.write('\
     <nav class="navbar">\
-        <div>\
+        <ul>\
             <li><a class="active" href="index.html">Home</a></li>\
             <li><a href="about.html">About Me</a></li>\
             <li><a href="resume.html">Resume</a></li>\
             <li><a href="https://github.com/ferguson-cole" target="_blank" rel="noopener noreferrer">Github</a></li>\
             <li><a href="contact.html">Contact</a></li>\
-        </div>\
+        </ul>\
     </nav>\
 ');
