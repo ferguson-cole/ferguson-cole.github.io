6a06507edf65d853a93a7404969de0c290cb336d
Cole <coleferguson9@gmail.com>
2021-08-08 16:54:55 -0700

    added javascript functionality to nav bar

--- navbar.js
+++ navbar.js
@@ -1,3 +1,5 @@
+$(document).ready(setActivePage);
+
 document.write('\
     <nav class="navbar">\
         <ul>\
@@ -5,7 +7,17 @@ document.write('\
             <li><a href="about.html">About Me</a></li>\
             <li><a href="resume.htm">Resume</a></li>\
             <li><a href="https://github.com/ferguson-cole" target="_blank" rel="noopener noreferrer">Github</a></li>\
-            <li><a href="contact.html">Contact</a></li>\
         </ul>\
     </nav>\
 ');
+
+function setActivePage() {
+    $('.navbar li a').click(function(e) {
+        $('.navbar li.active').removeClass('active');
+
+        $(this).parent().addClass('active');
+        e.preventDefault();
+    });
+}
+// add if contact page needed
+// <li><a href="contact.html">Contact</a></li>\
