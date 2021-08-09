b508a6c7834159128e1a9d4f220b40216748afef
Cole <coleferguson9@gmail.com>
2021-08-08 19:01:07 -0700

    css modifications

--- navbar.js
+++ navbar.js
@@ -1,23 +1,25 @@
-$(document).ready(setActivePage);
-
 document.write('\
     <nav class="navbar">\
         <ul>\
-            <li><a class="active" href="index.html">Home</a></li>\
-            <li><a href="about.html">About Me</a></li>\
-            <li><a href="resume.htm">Resume</a></li>\
+            <li><a id="home" href="home.html">Home</a></li>\
+            <li><a id="about" href="about.html">About Me</a></li>\
+            <li><a id="resume" href="resume.htm">Resume</a></li>\
             <li><a href="https://github.com/ferguson-cole" target="_blank" rel="noopener noreferrer">Github</a></li>\
         </ul>\
     </nav>\
 ');
 
-function setActivePage() {
-    $('.navbar li a').click(function(e) {
-        $('.navbar li.active').removeClass('active');
+var currentPage = window.location.href.toString();
 
-        $(this).parent().addClass('active');
-        e.preventDefault();
-    });
+if (currentPage.includes("about")) {
+    document.getElementById('about').className = "active";
+} else if (currentPage.includes("resume")) {
+    document.getElementById('resume').className = "active";
+} else {
+    document.getElementById('home').className = "active";
 }
+
+
+
 // add if contact page needed
 // <li><a href="contact.html">Contact</a></li>\
